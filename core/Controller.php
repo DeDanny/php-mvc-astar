2da08eac79e12a04deff7debf9e557e288d26d55
Danny <DeDanny@gmail.com>
2013-07-29 21:03:26 +0200

    404 handler unknown method

--- core/Controller.php
+++ core/Controller.php
@@ -1,10 +1,7 @@
 <?php
 
 abstract class Controller {
-    public function __call($name, $arguments) {
-        
-        var_dump($arguments);
-        
+    public function __call($name, $arguments) {    
         $arguments[1]->setHeader('HTTP/1.0 404 Not Found');
         return array('view' => '404');
     }
